c5f18c40cdddce67f49ee155cea78733a572997f
big <thitiphong479big@hotmail.com>
2026-01-10 20:43:01 +0700

    add apexchart

---
+++ shop-applications/components/chart/Pie.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-card
+      v-if="series.length > 0 && options"
+      variant="flat"
+      color="transprent"
+    >
+      <ClientOnly>
+        <apexchart
+          id="chart-pie"
+          type="pie"
+          :options="options"
+          :series="series"
+        ></apexchart>
+      </ClientOnly>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    series: Array,
+    options: Object,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ shop-applications/pages/dashboard/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-container>
+      <v-row dense>
+        <v-col cols="12" md="6">
+          <ChartPie :series="series" :options="options" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      series: [3, 5],
+      options: {
+        chart: {
+          type: "pie",
+          width: 100,
+          height: 100,
+          fontFamily: "Prompt, sans-serif",
+        },
+        colors: ["#4CAF50", "#FF9900"],
+        labels: ["ชำระเงินแล้ว", "ค้างชำระ"],
+        title: {
+          text: "",
+          align: "left",
+        },
+        legend: {
+          position: "top",
+        },
+      },
+    };
+  },
+};
+</script>
